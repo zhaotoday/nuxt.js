78c3b70661a6e1a79b654967d9287ffacb8ae482
zhaojintian <729234283@qq.com>
2019-01-27 19:12:46 +0800

    优化

--- store/articles/index.js
+++ store/articles/index.js
@@ -1,17 +1,10 @@
 import mutations from './mutations'
 import actions from './actions'
 
-const initState = {
-  list: {},
-  detail: {}
-}
-
 export default {
   state: () => ({
-    ...initState,
-    getInitState () {
-      return initState
-    }
+    list: {},
+    detail: {}
   }),
   mutations,
   actions,
