c401798ca6ee58e5b4e09e30ff07b6b5b437da0e
zhaojintian <729234283@qq.com>
2019-01-27 11:58:23 +0800

    安装 node-sass sass-loader

---
+++ pages/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    1111
+    <img src="./styles/images/logo.png">
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+  }
+}
+</script>
+
+<style
+  lang="scss"
+  src="./styles/index.scss">
+</style>
