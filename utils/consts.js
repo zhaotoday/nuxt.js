c1115a0a5f0b1863d187ee96b1ae14531e1dfb81
zhaojintian <729234283@qq.com>
2019-01-27 20:03:31 +0800

    配置常量

--- utils/consts.js
+++ utils/consts.js
@@ -1,6 +1,11 @@
+// 接口基础地址
 const API_BASE_URL = 'http://localhost:3002/v1/public'
 // const API_BASE_URL = 'https://api.cmsx.cn/v1/public'
 
+// 分页大小
+const PAGE_SIZE = 10
+
 export default {
-  API_BASE_URL
+  API_BASE_URL,
+  PAGE_SIZE
 }
