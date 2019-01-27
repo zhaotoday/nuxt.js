b425a07ab9785f997643a13a1f4a20d5336fbb12
zhaojintian <729234283@qq.com>
2019-01-27 16:35:15 +0800

    数据模型

---
+++ models/articles.js
@@ -0,0 +1,11 @@
+import REST from '~/utils/rest'
+import consts from '~/utils/consts'
+
+export default class extends REST {
+  constructor () {
+    super()
+
+    this.baseURL = consts.API_BASE_URL
+    this.path = 'articles'
+  }
+}
