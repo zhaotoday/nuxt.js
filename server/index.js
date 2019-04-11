f8ee7c2e68814c957e3730d97fbd46780022dd1a
zhaojintian <729234283@qq.com>
2019-04-11 17:10:59 +0800

    版本升级

--- server/index.js
+++ server/index.js
@@ -5,13 +5,13 @@ const { Nuxt, Builder } = require('nuxt')
 const app = new Koa()
 
 // Import and Set Nuxt.js options
-let config = require('../nuxt.config.js')
+const config = require('../nuxt.config.js')
 config.dev = !(app.env === 'production')
 
 async function start() {
   // Instantiate nuxt.js
   const nuxt = new Nuxt(config)
-  
+
   const {
     host = process.env.HOST || '127.0.0.1',
     port = process.env.PORT || 3000
@@ -21,9 +21,11 @@ async function start() {
   if (config.dev) {
     const builder = new Builder(nuxt)
     await builder.build()
+  } else {
+    await nuxt.ready()
   }
 
-  app.use(ctx => {
+  app.use((ctx) => {
     ctx.status = 200
     ctx.respond = false // Bypass Koa's built-in response handling
     ctx.req.ctx = ctx // This might be useful later on, e.g. in nuxtServerInit or with nuxt-stash
