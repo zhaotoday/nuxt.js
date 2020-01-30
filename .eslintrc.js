7c8b3d6a346612cf8393bed443d329daeb626312
zhaojintian <729234283@qq.com>
2020-01-30 10:07:15 +0800

    优化

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,6 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
+    'nuxt/no-cjs-in-config': 'off'
   }
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,5 @@
 
-export default {
+module.exports = {
   mode: 'universal',
   /*
   ** Headers of the page
