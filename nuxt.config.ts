56c8ebfc39a9e944d9d576e249e051d2d0058c73
zhaojintian <729234283@qq.com>
2022-08-17 17:49:40 +0800

    u

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,24 +2,9 @@ import { defineNuxtConfig } from "nuxt";
 
 export default defineNuxtConfig({
   srcDir: "src/",
-  dir: {
-    pages: "views",
-  },
   css: ["@/assets/styles/global/index.scss"],
   build: {
     extractCSS: true,
-  },
-  app: {
-    head: {
-      title: "福建省智慧医工联合工程研究中心",
-      meta: [
-        {
-          hid: "description",
-          name: "description",
-          content: "my app description",
-        },
-      ],
-    },
   },
   privateRuntimeConfig: {
     API_URL: process.env.API_URL,
