44cc9d7b6aac1cdf6e82d6c36cc35592b73a42e6
zhaojintian <729234283@qq.com>
2022-08-19 17:53:43 +0800

    u

---
+++ src/pages/index/components/schemes/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <ul class="b-schemes">
+    <li></li>
+  </ul>
+</template>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -5,13 +5,17 @@
     </div>
     <div class="bg-white">
       <div class="c-block c-container">
-        <div class="c-block__title">地方大师傅</div>
-        <div class="c-block__extra">的高富帅的规定</div>
+        <div class="c-block__title">小程序+APP+代运营+精准营销解决方案</div>
+        <div class="c-block__extra">
+          从软件开发到营销，为你打造一站式解决方案
+        </div>
       </div>
     </div>
     <div class="c-block c-container">
-      <div class="c-block__title">地方大师傅</div>
-      <div class="c-block__extra">的高富帅的规定</div>
+      <div class="c-block__title">服务优势</div>
+      <div class="c-block__extra">
+        快速打造个性化软件，享受安全、稳定的云服务
+      </div>
     </div>
   </div>
 </template>
