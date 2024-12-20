2531fbfe2312be434b5789f8f5de6e247c16fe20
zhaojintian <729234283@qq.com>
2024-12-20 20:34:08 +0800

    备份

--- .nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, Ref } from 'vue'
-export type LayoutKey = "default"
-declare module "D:/Projects/Github/nuxt.js/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
-  }
-}
